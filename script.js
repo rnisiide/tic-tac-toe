55b36a76cf727823f8a488c1dd17f9b674569e62
Ricardo Nisiide <rnisiide@gmail.com>
2024-04-06 17:13:31 -0300

    added logic for winning and stopping game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const board = [
-    rowOne = ['', '', ''],
-    rowTwo = ['', '', ''],
-    rowThree = ['', '', '']
+    ['-', '-', '-'],
+    ['-', '-', '-'],
+    ['-', '-', '-']
 ]
 
 const playerOne = {
@@ -25,12 +25,54 @@ function showClick(row, column) {
     const selector = document.getElementById(row + "-" + column);
     console.log('you clicked row ' + row + ' and column ' + column);
     selector.innerText = currentPlayer.marker;
+    selector.style.cssText = "background-color: grey; pointer-events:none";
+    board[row][column] = currentPlayer.marker;
+    checkWinner();
     changePlayer();
-
 }
 
 
 function changePlayer() {
     if (currentPlayer == playerOne) { currentPlayer = playerTwo } else { currentPlayer = playerOne }
     turn.innerText = currentPlayer.name + "'s turn, with the marker " + currentPlayer.marker;
+}
+
+const winner = document.getElementById('winner');
+function checkWinner() {
+    if (
+        //rows
+        (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') ||
+        (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') ||
+        (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') ||
+        //columns
+        (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') ||
+        (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') ||
+        (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') ||
+        //diagonals
+        (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') ||
+        (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X')
+    ) {
+        winner.innerText = 'Player One wins';
+        const endgame = document.querySelectorAll('.board-item');
+        console.log(endgame);
+        for (let i = 0; i < endgame.length; i++) {
+            endgame[i].style.cssText = "background-color: grey; pointer-events:none";
+        }
+    } else if (
+        //rows
+        (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O') ||
+        (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O') ||
+        (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O') ||
+        //columns
+        (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O') ||
+        (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O') ||
+        (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O') ||
+        //diagonals
+        (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') ||
+        (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O')
+
+    ) {
+        winner.innerText = 'Player Two wins';
+    } else { winner.innerText = 'Nobody has won yet' }
+
 }
