c84707e9cc3ab2c8e281a69df2ad2c4187e04615
Ricardo Nisiide <rnisiide@gmail.com>
2024-04-08 23:35:43 -0300

    cleaned up the code, eg made object and function factories, organized logic better. added funcionalities to changing players names

--- script.js
+++ script.js
@@ -1,78 +1,139 @@
-const board = [
-    ['-', '-', '-'],
-    ['-', '-', '-'],
-    ['-', '-', '-']
-]
+function Player(name, marker, color, score) {
+    let playerName = name;
+    score = 0;
+    this.marker = marker;
+    this.color = color;
+    const SetPlayerName = (newName) => playerName = newName;
+    const GetPlayerName = () => playerName;
+    const GetPlayerMarker = () => marker;
+    const GetColor = () => color;
+    const SetScore = () => score++;
+    const GetScore = () => score;
+    return {
+        SetPlayerName, GetPlayerName, GetPlayerMarker, GetColor, GetScore, SetScore
+    }
 
-const playerOne = {
-    name: 'Player One',
-    marker: 'X'
 };
 
-const playerTwo = {
-    name: 'Player Two',
-    marker: 'O'
-};
+const PlayGame = (function () {
+
+    const board = [
+        '-', '-', '-',
+        '-', '-', '-',
+        '-', '-', '-'
+    ]
 
 
-let currentPlayer = playerOne;
+    const playerOne = new Player('Player One', 'X', 'blue');
+    const playerTwo = new Player('Player Two', 'O', 'red');
+    let currentPlayer = playerOne;
+    let matchOngoing = true;
 
-const turn = document.getElementById('turn');
-turn.innerText = currentPlayer.name + "'s turn, with the marker " + currentPlayer.marker;
+    const p1Score = document.getElementById('p1-score');
+    const p2Score = document.getElementById('p2-score');
+    const winner = document.getElementById('winner');
+    const turn = document.getElementById('turn');
+    const p1name = document.getElementById('p1-name');
+    const p2name = document.getElementById('p2-name');
+    const p1input = document.getElementById('p1-input');
+    const p2input = document.getElementById('p2-input');
+    p1input.addEventListener('change', function () { changeName(p1input.value, playerOne) })
+    p2input.addEventListener('change', function () { changeName(p2input.value, playerTwo) })
+
+    function changeName(newName, newPlayer) {
+        newPlayer.SetPlayerName(newName);
+        p1name.innerText = playerOne.GetPlayerName() + ': ';
+        p2name.innerText = playerTwo.GetPlayerName() + ': ';
+        turn.innerText = currentPlayer.GetPlayerName() + "'s turn, with the marker " + currentPlayer.GetPlayerMarker();
+        p1input.value = '';
+        p2input.value = '';
+
+    }
 
 
-function showClick(row, column) {
-    const selector = document.getElementById(row + "-" + column);
-    console.log('you clicked row ' + row + ' and column ' + column);
-    selector.innerText = currentPlayer.marker;
-    selector.style.cssText = "background-color: grey; pointer-events:none";
-    board[row][column] = currentPlayer.marker;
-    checkWinner();
-    changePlayer();
-}
-
-
-function changePlayer() {
-    if (currentPlayer == playerOne) { currentPlayer = playerTwo } else { currentPlayer = playerOne }
-    turn.innerText = currentPlayer.name + "'s turn, with the marker " + currentPlayer.marker;
-}
-
-const winner = document.getElementById('winner');
-function checkWinner() {
-    if (
-        //rows
-        (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') ||
-        (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') ||
-        (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') ||
-        //columns
-        (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') ||
-        (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') ||
-        (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') ||
-        //diagonals
-        (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') ||
-        (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X')
-    ) {
-        winner.innerText = 'Player One wins';
-        const endgame = document.querySelectorAll('.board-item');
-        console.log(endgame);
-        for (let i = 0; i < endgame.length; i++) {
-            endgame[i].style.cssText = "background-color: grey; pointer-events:none";
+    function changePlayer() {
+        if (matchOngoing == true) {
+            if (currentPlayer == playerOne) { currentPlayer = playerTwo } else { currentPlayer = playerOne }
+            turn.innerText = currentPlayer.GetPlayerName() + "'s turn, with the marker " + currentPlayer.GetPlayerMarker();
         }
-    } else if (
-        //rows
-        (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O') ||
-        (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O') ||
-        (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O') ||
-        //columns
-        (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O') ||
-        (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O') ||
-        (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O') ||
-        //diagonals
-        (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') ||
-        (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O')
+    }
 
-    ) {
-        winner.innerText = 'Player Two wins';
-    } else { winner.innerText = 'Nobody has won yet' }
+    const buttons = document.querySelectorAll('.game-button')
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function () { placeMarker(i) });
+    }
 
-}
+    const placeMarker = (cell) => {
+        console.log('you clicked on cell ' + cell);
+        buttons[cell].innerText = currentPlayer.GetPlayerMarker();
+        buttons[cell].style.cssText = "background-color: " + currentPlayer.GetColor() + "; pointer-events:none";
+        board[cell] = currentPlayer.GetPlayerMarker();
+        checkWinner();
+        changePlayer();
+    }
+
+
+
+
+    function checkWinner() {
+        const winningConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        if (!board.includes('-')) {
+            winner.innerText = "It's a draw!";
+            endGame();
+        } else {
+            for (let i = 0; i < winningConditions.length; i++)
+                if (board[winningConditions[i][0]] == currentPlayer.GetPlayerMarker() &&
+                    board[winningConditions[i][1]] == currentPlayer.GetPlayerMarker() &&
+                    board[winningConditions[i][2]] == currentPlayer.GetPlayerMarker()) {
+                    winner.innerText = currentPlayer.GetPlayerName() + ' wins!';
+                    currentPlayer.SetScore();
+                    endGame();
+                }
+        }
+    }
+
+    function endGame() {
+        matchOngoing = false;
+        turn.innerText = 'The game has ended. Would you like to restart?';
+        p1Score.innerText = playerOne.GetScore();
+        p2Score.innerText = playerTwo.GetScore();
+        for (let i = 0; i < buttons.length; i++)
+            if (
+                !(buttons[i].innerText == playerOne.GetPlayerMarker()) &&
+                !(buttons[i].innerText == playerTwo.GetPlayerMarker())) {
+                buttons[i].style.cssText = "background-color: grey; pointer-events:none";
+            }
+
+    }
+
+})
+PlayGame();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
